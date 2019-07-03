0240b343f7baad9ec8b2fb612a34cd07e080fd25
skywalkerwei <skywalkerwei@gmail.com>
2019-07-03 10:08:32 +0800

    config fix

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 return [
     'title' => "APi接口文档",  //文档title
     'version'=>'1.0.0', //文档版本
-    'copyright'=>'Powered By Zhangweiwei', //版权信息
+    'copyright'=>'Powered By ypai', //版权信息
     'password' => '', //访问密码，为空不需要密码
     //静态资源路径--默认为云上路径，解决很多人nginx配置问题
     //可将assets目录拷贝到public下面，具体路径课自行配置
